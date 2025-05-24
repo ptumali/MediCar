0fd59f2014580f4df11988295deb9a0558e814a1
Yoga Hadinata <yogahadinata@Yogas-MacBook-Pro.local>
2025-05-24 12:34:04 -0700

    making second page, z-index still funky

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,13 @@ import './globals.css';
 
 export const metadata = {
   title: 'Medi-Car',
-  description: 'Car Diagnosis Web App'
+  description: 'Car Diagnosis Web App',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white text-black min-h-screen">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
