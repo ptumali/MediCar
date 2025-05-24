789f029144bb54bb7d225dc0a34d0fe8f03257d7
Yoga Hadinata <yogahadinata@dhcp-172-24-250-170.mobile.uci.edu>
2025-05-24 13:40:11 -0700

    dropdown works no vin box yet

---
+++ src/app/vehicle/page.jsx
@@ -0,0 +1,147 @@
+'use client';
+import { useState, useEffect } from 'react';
+import styles from './vehicle.module.css';
+
+const MAKES = [
+  'Acura', 'Alfa Romeo', 'Audi', 'BMW', 'Chevrolet', 'Chrysler', 'Dodge',
+  'Ford', 'Genesis', 'Honda', 'Hyundai', 'Jaguar', 'Jeep', 'Kia', 'Lexus',
+  'Mazda', 'Mercedes-Benz', 'Mini', 'Mitsubishi', 'Nissan', 'Subaru', 'Tesla',
+  'Toyota', 'Volkswagen', 'Volvo'
+];
+
+const MODELS_BY_MAKE = {
+  Acura: ['ILX', 'MDX', 'RDX', 'TLX'],
+  'Alfa Romeo': ['Giulia', 'Stelvio'],
+  Audi: ['A3', 'A4', 'Q5', 'Q7', 'A6', 'e-tron'],
+  BMW: ['3 Series', '5 Series', 'X3', 'X5'],
+  Chevrolet: ['Cruze', 'Malibu', 'Tahoe', 'Equinox'],
+  Chrysler: ['300', 'Pacifica'],
+  Dodge: ['Charger', 'Challenger', 'Durango'],
+  Ford: ['F-150', 'Escape', 'Explorer', 'Mustang'],
+  Genesis: ['G70', 'G80', 'GV70', 'GV80'],
+  Honda: ['Accord', 'Civic', 'CR-V', 'Pilot'],
+  Hyundai: ['Elantra', 'Santa Fe', 'Tucson', 'Sonata'],
+  Jaguar: ['F-Pace', 'XE', 'XF'],
+  Jeep: ['Cherokee', 'Grand Cherokee', 'Wrangler'],
+  Kia: ['Forte', 'Sorento', 'Sportage', 'Telluride'],
+  Lexus: ['ES', 'IS', 'RX', 'NX'],
+  Mazda: ['Mazda 2', 'Mazda 3', 'Mazda 5', 'CX-5', 'CX-9', 'MX-5 Miata'],
+  'Mercedes-Benz': ['C-Class', 'E-Class', 'GLC', 'GLE'],
+  Mini: ['Cooper', 'Clubman', 'Countryman'],
+  Mitsubishi: ['Outlander', 'Eclipse Cross'],
+  Nissan: ['Altima', 'Rogue', 'Sentra', 'Pathfinder'],
+  Subaru: ['Impreza', 'Forester', 'Outback', 'Crosstrek'],
+  Tesla: ['Model 3', 'Model S', 'Model X', 'Model Y'],
+  Toyota: ['Camry', 'Corolla', 'RAV4', 'Highlander'],
+  Volkswagen: ['Golf', 'Passat', 'Tiguan', 'Atlas'],
+  Volvo: ['XC40', 'XC60', 'XC90', 'S60']
+};
+
+const YEARS = ['2025', '2024', '2023', '2022', '2021'];
+
+export default function VehiclePage() {
+  const [make, setMake] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState('');
+
+  const sendToBackend = (make, model, year) => {
+    console.log('Sending to backend:', { make, model, year });
+  };
+
+  const handleSubmit = () => {
+    sendToBackend(make, model, year);
+  };
+
+  function SearchSelect({ label, options, value, onSelect }) {
+    const [input, setInput] = useState(value || '');
+    const [filtered, setFiltered] = useState(options);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    useEffect(() => {
+      setInput(value || '');
+      setFiltered(options);
+    }, [value, options]);
+
+    const handleChange = (e) => {
+      const val = e.target.value;
+      setInput(val);
+      setFiltered(options.filter((opt) =>
+        opt.toLowerCase().includes(val.toLowerCase())
+      ));
+      setShowDropdown(true);
+    };
+
+    const handleSelect = (val) => {
+      onSelect(val);
+      setShowDropdown(false);
+    };
+
+    return (
+      <div className={showDropdown ? styles.selectWrapperFocused : styles.selectWrapper}>
+        <div className={styles.container}>
+          <label className={styles.label}>{label}</label>
+          <input
+            className={styles.input}
+            type="text"
+            value={input}
+            onChange={handleChange}
+            onFocus={() => setShowDropdown(true)}
+            onBlur={() => setTimeout(() => setShowDropdown(false), 100)}
+            placeholder={`Search ${label.toLowerCase()}...`}
+          />
+          {showDropdown && (
+            <ul className={styles.dropdown}>
+              {filtered.map((item, idx) => (
+                <li key={idx} className={styles.option} onClick={() => handleSelect(item)}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <main className={styles.page}>
+      <h1 className={styles.heading}>Help us specify your vehicle:</h1>
+
+      <div className={styles.searchColumn}>
+        <SearchSelect
+          label="Make"
+          options={MAKES}
+          value={make}
+          onSelect={(val) => {
+            setMake(val);
+            setModel('');
+            setYear('');
+          }}
+        />
+
+        <SearchSelect
+          label="Model"
+          options={make ? MODELS_BY_MAKE[make] || [] : []}
+          value={model}
+          onSelect={(val) => {
+            setModel(val);
+            setYear('');
+          }}
+        />
+
+        <SearchSelect
+          label="Year"
+          options={model ? YEARS : []}
+          value={year}
+          onSelect={setYear}
+        />
+      </div>
+
+      {make && model && year && (
+        <button onClick={handleSubmit} className={styles.button}>
+          Continue
+        </button>
+      )}
+    </main>
+  );
+}
